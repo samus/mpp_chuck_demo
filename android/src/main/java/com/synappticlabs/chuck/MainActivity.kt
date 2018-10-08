384376a3da0b5c0c07c62edef0211ec03ee10fa5
Sam Corder <samus@codeargyle.com>
2018-10-08 00:48:13 -0400

    Working to move the android app to it's own sub directory.

--- android/src/main/java/com/synappticlabs/chuck/MainActivity.kt
+++ android/src/main/java/com/synappticlabs/chuck/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.synappticlabs.chuck
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+
+//import com.synappticlabs.chuck.network.JokeApiClient
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
